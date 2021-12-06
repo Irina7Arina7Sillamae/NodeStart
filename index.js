ef6103922ea6d7ad4fd66e17772ac12b93819e73
Irina <Irina.Siskova@ivkhk.ee>
2021-12-06 13:45:03 +0200

    9.Simple Web-server

--- index.js
+++ index.js
@@ -1,74 +1,21 @@
 const http = require ('http')
-const path = require ('path')
-const fs = require ('fs')
 
-//Cоздание своего сервера
+//Cоздание сервера
+//Чтения данных с сайта, доступ к указанной странице
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
         res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
+            'Content-Type': 'text/html'
         })
-//выход на главную страницу
-        if (req.url === '/') {
-            fs.readFile(
-                path.join(__dirname, 'views',  'index.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) { 
-                        throw err
-                    }
-
-                    res.end(content)
-                }
-            )
-//выход на страницу About
-        } else if (req.url === '/about') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'about.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
-                    res.end(content)
-                }
-            )
-//выход на страницу Art
-        } else if (req.url === '/art') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'art.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
-                    res.end(content)
-                }
-            )
-        } else if (req.url === '/animals') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'animals.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
-                    res.end(content)
-                }
-            )
-//выход на страницу Architec
-        } else if (req.url === '/architec') {
-            fs.readFile(
-                path.join(__dirname, 'views', 'architec.html'),
-                'utf-8',
-                (err, content) => {
-                    if (err) {
-                        throw err
-                    }
-                    res.end(content)
-                }
-            )
-        }
+//Форма для принятия сообщения
+        res.end(`
+            <h1>Form</h1>
+            <form method = "post" action="/">
+                <input name = "title" type = "text" />
+                <button type = "submit">Send</button>
+            </form>  
+        `) 
+//Mетод для отправки данных на сайт  
     } else if (req.method === 'POST') {
         const body = []
         res.writeHead(200, {
@@ -78,8 +25,8 @@ const server = http.createServer((req, res) => {
         req.on('data', data => {
             body.push(Buffer.from(data))
         })
-
-        req,on('end', () => {
+//вывод сообщения
+        req.on('end', () => {
             const message = body.toString().split('=')[1]
 
             res.end(`
@@ -88,7 +35,7 @@ const server = http.createServer((req, res) => {
         })
     }
 })
-        
+
 //сервер слушает порт 3000
 server.listen(3000, () => {
     console.log('Server is running...')
