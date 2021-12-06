2375bb8645d9b51655d3232a5301d5911a1b0386
Irina <Irina.Siskova@ivkhk.ee>
2021-12-06 13:55:16 +0200

    7_HTTP

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
 const http = require ('http')
 
-//Cоздание сервера
-//Чтения данных с сайта, доступ к указанной странице
+//Cоздание своего сервера
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        })
-//Форма для принятия сообщения
-        res.end(`
-            <h1>Form</h1>
-            <form method = "post" action="/">
-                <input name = "title" type = "text" />
-                <button type = "submit">Send</button>
-            </form>  
-        `) 
-//Mетод для отправки данных на сайт  
-    } else if (req.method === 'POST') {
-        const body = []
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        })
+    console.log(req.url)
 
-        req.on('data', data => {
-            body.push(Buffer.from(data))
-        })
-//вывод сообщения
-        req.on('end', () => {
-            const message = body.toString().split('=')[1]
-
-            res.end(`
-            <h1>Ваше сообщение: ${message}</h1>
-            `)
-        })
-    }
+    res.write('<h1>Hello from NodeJS</h1>')
+    res.write('<h2>Hello from NodeJS</h2>')
+    res.write('<h3>Hello from NodeJS</h3>')
+    res.end(`
+    <div style="background: #f4e539; width: 400px; height: 200px;">
+    <h1>Test</h1>
+    </div>
+    
+    <img src="https://klike.net/uploads/posts/2019-05/1556708032_1.jpg" style=" width: 400px; height: 200px" >
+    
+    <div style="background: #fe876c ; width: 400px; height: 200px; padding-top: 50px;">
+    <div style="text-align: center;  font-size: 20px;">Aspirations subsided; different beauty
+    <br>Changed the old one; jubilant summer
+    <br>Strong rays are no longer warmed,
+    <br>Nature is all full of the last warmth;
+    <br>Even along the wet between the flowers flaunt.
+    <br>Alexey Tolstoy
+    </div>
+    </div>
+    `)
 })
 
 //сервер слушает порт 3000
-server.listen(3000, () => {
+ server.listen(3000, () => {
     console.log('Server is running...')
 })
